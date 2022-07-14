d28bdb2600659eb10034bddfdca9a8c9d1e59993
于泽佳-Zejia <zejia.yu@d1m.cn>
2022-07-14 13:06:38 +0800

    jsdoc

---
+++ README.md
@@ -0,0 +1,3 @@
+# 使用jsDoc来生成文档
+## Hello World示例
+这里是JSDoc根页面
--- packages/utils/index.js
+++ packages/utils/index.js
@@ -1,1 +1,6 @@
+/**
+ * 检测对象是否为空
+ * @param {*} obj 
+ * @returns 
+ */
 const isEmpty = obj => Reflect.ownKeys(obj).length === 0 && obj.constructor === Object
